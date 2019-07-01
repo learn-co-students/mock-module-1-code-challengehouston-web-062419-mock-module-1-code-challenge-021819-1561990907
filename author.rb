8f2dc059a8e4cbd82b700edbf23b6ed9bcf37d81
github username <github email address>
2019-07-01 10:35:01 -0500

    unfinished

--- author.rb
+++ author.rb
@@ -1,1 +1,42 @@
+class Author    
 
+    attr_accessor :author, :title, :word_count, :book
+    attr_reader
+    attr_writer
+
+    @@all = []
+
+    def initialize(author, title, word_count)
+        @author = author
+        @title = title
+        @word_count = word_count
+        # @book = book 
+	   
+	    @@all << self
+    end
+
+    def self.all
+    	@@all
+    end
+
+    def books
+        Book_author.all.select do |book_author|
+            book_author.book == self
+          end
+    end
+
+    def write_book(title, word_count)
+        Book.new(self, title, word_count)
+    end
+
+    def total_words
+        books.map do |word|
+            word.word_count
+        end
+    end
+
+    def self.most_words
+    end
+
+
+end
--- book_author.rb
+++ book_author.rb
@@ -1,1 +1,21 @@
+class Book_author    
+
+    attr_accessor :book, :author, :title, :word_count
+    attr_reader
+    attr_writer
+
+    @@all = []
+
+    def initialize(book, author, title, word_count)
+	    @book = book
+        @author = author
+        @title = title
+        @word_count = word_count
+	    @@all << self
+    end
+
+    def self.all
+    	@@all
+    end
+end
 
