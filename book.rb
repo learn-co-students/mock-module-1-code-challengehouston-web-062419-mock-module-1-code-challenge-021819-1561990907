7d89edd47b40d891b30408f70185ac3b23453255
jordanbacon <jmbacon92@gmail.com>
2019-07-01 08:36:43 -0700

    mock

--- book.rb
+++ book.rb
@@ -1,1 +1,34 @@
+class Book
 
+    attr_reader :title
+    attr_accessor :author, :bookauthor
+
+    @@all = []
+
+    def initialize(title, author, bookauthor)
+        @title = title
+        @author = author
+        @bookauthor = bookauthor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def authors
+        Book.all.select do |book|
+            book.author
+        end
+
+    def title
+        self.authors.map do |title|
+            title.book
+    end
+
+    def word_count
+        self.authors.map do |word_count|
+            word_count.book
+    end
+    
+end
